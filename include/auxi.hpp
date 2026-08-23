ffc748bf1d532cfcdb7d3b02c26c4ea391647d9e
Powerpowername <1361581236@qq.com>
2026-08-24 00:04:07 +0800

    删除PhysX的通用删除器，PhysX有自己的内存管理，不适合用智能指针管理

--- include/auxi.hpp
+++ include/auxi.hpp
@@ -65,13 +65,4 @@ inline physx::PxMat44 toPxMat44(const glm::mat4& m)
         physx::PxVec4(m[3][0], m[3][1], m[3][2], m[3][3]));
 }
 
-template <typename T>
-inline void PxDeleter(T* t)
-{
-    if (t)
-    {
-        t->release();
-        t = nullptr;
-    }
-}
 } // namespace auxi
